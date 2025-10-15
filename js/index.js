c974d0ef8103b243ce67544a7059db7eff0e60c4
Pvirgilio <pvvirg16@gmail.com>
2025-10-15 10:55:21 -0300

    feat: adjustment on menu click

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-
-
 const btnMenu = document.getElementById("btnMenu");
 
 const containerMenu = document.getElementById("containerMenu");
 
 function menubar() {
-  if (window.matchMedia("(max-width: 767px)")) {
-    if (containerMenu.style.display === "none") {
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    // Obtém o estilo computado atual do elemento (incluindo CSS)
+    const displayStyle = window.getComputedStyle(containerMenu).display;
+
+    if (displayStyle === "none") {
       containerMenu.style.overflow = "hidden";
       containerMenu.style.display = "flex";
     } else {
       containerMenu.style.display = "none";
     }
   }
-};
+}
 
 function scrollToTop() {
   window.scrollTo({
